e190c6bafd4400a1750f25b3d5ea15fc4475cb53
ARGoncharov <gonlex@mail.ru>
2023-02-28 13:08:58 +0300

    Adding models

---
+++ RickAndMortyApp/Models/RMOrigin.swift
@@ -0,0 +1,13 @@
+//
+//  RMOrigin.swift
+//  RickAndMortyApp
+//
+//  Created by Алексей  on 28.02.2023.
+//
+
+import Foundation
+
+struct RMOrigin: Codable {
+    let name: String
+    let url: String
+}
---
+++ RickAndMortyApp/Models/RMSingleLocation.swift
@@ -0,0 +1,13 @@
+//
+//  RMSingleLocation.swift
+//  RickAndMortyApp
+//
+//  Created by Алексей  on 28.02.2023.
+//
+
+import Foundation
+
+struct RMSingleLocation: Codable {
+    let name: String
+    let url: String
+}
